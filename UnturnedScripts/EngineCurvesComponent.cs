9c99123d0827187e77101fa440104dae2a95150b
Wittummm <95731023+Wittummm@users.noreply.github.com>
2024-11-15 23:05:10 +0700

    Delete UnturnedScripts directory

--- UnturnedScripts/EngineCurvesComponent.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace SDG.Unturned;
-
-public class EngineCurvesComponent : MonoBehaviour
-{
-    [Tooltip("Maps normalized engine RPM to torque multiplier.\nIdle RPM is zero and max RPM is one on the X axis.")]
-    public AnimationCurve engineRpmToTorqueCurve;
-}
